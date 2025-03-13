d6c53960134df3cc7a443ac7ec07215c9aa541c7
Hoang Trung <nqhtrung304@gmail.com>
2025-03-13 22:54:36 +0700

    Finished Init function

--- main.cpp
+++ main.cpp
@@ -5,12 +5,16 @@ using namespace std;
 struct Student;
 struct Grade;
 struct Subject;
+ifstream studentFileR("StudentsList.txt");
+ofstream studentFileW("StudentsList.txt");
+ifstream subjectFileR("SubjectsList.txt");
+ofstream subjectFileW("SubjectsList.txt");
 
 // Function
 void Init();
-void addStudent();
-void addSubject();
+void addNode();
 void getGrade();
+void saveAndQuit();
 
 int main() {
     Init();
@@ -21,8 +25,11 @@ struct Grade {
     Grade *next;
     int IDST;
     string fullName;
-    double lab1, lab2, progressTest1 , progressTest1, finalTest;
-    double averageTen, averageFour;
+    double grades[8]; // This array indicate for
+    // lab1, lab2, progressTest1 , progressTest1, finalTest;
+    // averageTen, averageFour;
+    // If this student does not have this component grade yet, it will set -1 by default 
+    Grade(): next(NULL), IDST(0), fullName("") {}; // Default constructor
     Grade(); // Constructor
     void calAaverage();
     void transformToAlphabet();
@@ -34,24 +41,98 @@ struct Subject {
     string name;
     Subject(): next(NULL), IDS(0), name("") {}; // Default constructor
     Subject(); // Constructor
-    Grade *parentGradeNode; // Linked list of grade of each student
+    Grade *parentGradeNode = new Grade(); // Linked list of grade of each student
     void sortGrade();
     void showGradeList();
 };
-Subject *currentSubject = new Subject();
+Subject *subjectList = new Subject();
 
 struct Student {
     Student *next;
     int IDST;
-    Subject *parentSubjectNode = currentSubject; // Linked list of subject, it refers to global Linked list of Subject
+    Subject *parentSubjectNode = subjectList; // Linked list of subject, it refers to global Linked list of Subject
     string fullName;
     Student(): next(NULL), IDST(0), fullName("") {}; // Default constructor 
     Student(); // Constructor
     void modifyGrade();
     void showGrade();
 };
-Student *currentStudent = new Student();
+Student *studentList = new Student();
+
+void addNode(auto *element, auto *parentNode) {
+    auto *temp = parentNode;
+    while(temp->next != NULL) temp = temp->next;
+    temp-> next = element;
+}
 
 void Init() {
-
+    string line, temp, name;
+    int len;
+    // Get student data from file
+    Student *st = new Student();
+    if(studentFileR) { // Existing this file
+        while(getline(studentFileR, line)) {
+            temp = "", name = "";
+            len = line.size();
+            for(int i=0;i<len;i++) {
+                if(line[i] == ';') {
+                    name = temp;
+                    temp = "";
+                } else temp = temp + line[i];
+            }
+            st->fullName = name;
+            st->IDST = stoi(temp);
+            addNode(st, studentList);
+        }
+    } else {    
+        cout << "Cannot access to StudentList!";
+        exit(0);
+    }
+    // Get subject data from file
+    Subject *su = new Subject();
+    if(subjectFileR) { // Existing this file
+        while(getline(subjectFileR, line)) {
+            temp = "", name = "";
+            len = line.size();
+            for(int i=0;i<len;i++) {
+                if(line[i] == ';') {
+                    name = temp;
+                    temp = "";
+                } else temp = temp + line[i];
+            }
+            su->name = name; name = name + ".txt";
+            su->IDS = stoi(temp);
+            // Reading everysingle line data of this subject
+            ifstream gradesList(name);
+            string temp1, line1;
+            int len1, count = 1;
+            while(getline(gradesList, line1)) {
+                Grade *gr = new Grade();
+                len1 = line1.size();
+                for(int j=0;j<len1;j++) {
+                    if(line1[j] == ';') {
+                        if(count == 1) {
+                            gr->IDST = stoi(temp1);
+                            temp1 = "";
+                            count++;
+                        } else if(count == 2) {
+                            gr->fullName = temp1;
+                            temp1 = "";
+                            count = 1;
+                        } else {
+                            gr->grades[count] = stod(temp1);
+                            temp1 = "";
+                            count++;
+                        }
+                    } else temp1 = temp1 + line1[j];
+                } 
+                addNode(gr, su->parentGradeNode);
+            }
+        }
+        // Adding this subject to linked list
+        addNode(su, subjectList);
+    } else {
+        cout << "Cannot accest to SubjectsList!";
+        exit(0);
+    }
 }
